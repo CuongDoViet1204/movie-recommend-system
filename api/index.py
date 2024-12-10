626209febb8040b670553b53ce8de10944eed9cb
CuongDoViet1204 <101694969+CuongDoViet1204@users.noreply.github.com>
2024-12-10 12:23:19 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,16 @@
+import pandas as pd
+
+import mysql.connector
+
+import numpy as np
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+import pickle
+
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
 
